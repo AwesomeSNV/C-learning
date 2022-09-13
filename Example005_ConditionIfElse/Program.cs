960a5f85ed88dbe06f5ff702552e0694b8d3de88
Nikolaj <mr.nik.simonov@mail.ru>
2022-09-14 00:16:52 +0700

    add Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "николай")
+{
+    Console.WriteLine("Ура, это же НИИИКОЛАЙ!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
